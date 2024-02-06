47f5529bba02e77d374fc6e81adb9dee2d1f16ef
cdrmack <git.pynvt@passmail.net>
2024-02-06 08:13:07 +0100

    move from camelCase to snake_case for variables

--- main.zig
+++ main.zig
@@ -6,21 +6,21 @@ const ray = @cImport({
 const Ball = struct { position: ray.Vector2, radius: f32, color: ray.Color };
 
 pub fn main() void {
-    const screenWidth = 400;
-    const screenHeight = 800;
+    const screen_width = 400;
+    const screen_height = 800;
 
-    ray.InitWindow(screenWidth, screenHeight, "zbreakout");
+    ray.InitWindow(screen_width, screen_height, "zbreakout");
     defer ray.CloseWindow();
 
     ray.SetTargetFPS(60);
 
-    const startPosition = ray.Vector2{
-        .x = screenWidth / 2,
-        .y = screenHeight - 50, // TODO(cdrmack), remove magic number
+    const start_position = ray.Vector2{
+        .x = screen_width / 2,
+        .y = screen_height - 50, // TODO(cdrmack), remove magic number
     };
 
     var ball = Ball{
-        .position = startPosition,
+        .position = start_position,
         .radius = 8,
         .color = ray.RED,
     };
