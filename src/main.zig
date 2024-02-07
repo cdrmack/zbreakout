826216eb33b9b08d00e3b5c17cf35e3130efd676
cdrmack <git.pynvt@passmail.net>
2024-02-07 10:24:32 +0100

    add first Brick

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,36 @@ const ray = @cImport({
     @cInclude("raylib.h");
 });
 
-const screen_width = 400;
+const screen_width = 600;
 const screen_height = 400;
+
 const player_width = 100;
-const player_height = 10;
+const player_height = 20;
+
+const brick_width = player_width;
+const brick_height = player_height;
 
 const Ball = struct { position: ray.Vector2, radius: f32, color: ray.Color };
 const Player = struct { rectangle: ray.Rectangle, color: ray.Color };
+const Brick = struct { rectangle: ray.Rectangle, color: ray.Color };
 
 const Direction = enum { left, right };
+
+pub fn makeVector2(x: f32, y: f32) ray.Vector2 {
+    return ray.Vector2{
+        .x = x,
+        .y = y,
+    };
+}
+
+pub fn makeRectangle(x: f32, y: f32, width: f32, height: f32) ray.Rectangle {
+    return ray.Rectangle{
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+    };
+}
 
 pub fn playerCanMove(player: *Player, direction: Direction) bool {
     if (direction == Direction.left) {
@@ -35,22 +56,6 @@ pub fn input(player: *Player) void {
     }
 }
 
-pub fn makeVector2(x: f32, y: f32) ray.Vector2 {
-    return ray.Vector2{
-        .x = x,
-        .y = y,
-    };
-}
-
-pub fn makeRectangle(x: f32, y: f32, width: f32, height: f32) ray.Rectangle {
-    return ray.Rectangle{
-        .x = x,
-        .y = y,
-        .width = width,
-        .height = height,
-    };
-}
-
 pub fn main() void {
     ray.InitWindow(screen_width, screen_height, "zbreakout");
     defer ray.CloseWindow();
@@ -68,6 +73,11 @@ pub fn main() void {
         .color = ray.GREEN,
     };
 
+    var brick = Brick{
+        .rectangle = makeRectangle(50, 50, brick_width, brick_height),
+        .color = ray.BLUE,
+    };
+
     while (!ray.WindowShouldClose()) {
         // input
         input(&player);
@@ -79,5 +89,6 @@ pub fn main() void {
         ray.ClearBackground(ray.BLACK);
         ray.DrawCircleV(ball.position, ball.radius, ball.color);
         ray.DrawRectangleRec(player.rectangle, player.color);
+        ray.DrawRectangleRec(brick.rectangle, brick.color);
     }
 }
