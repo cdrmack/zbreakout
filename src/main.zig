7fb788274ec6f314336fb5fdc79664f4ee7d25cf
cdrmack <git.pynvt@passmail.net>
2024-02-15 16:18:54 +0100

    move ball related functions inside ball struct

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,68 @@ const brick_gap = (screen_width - (bricks_per_row * brick_width)) / (bricks_per_
 
 const ball_radius = 8;
 
-const Ball = struct { position: ray.Vector2, velocity: ray.Vector2, radius: f32, color: ray.Color };
+const Ball = struct {
+    position: ray.Vector2,
+    velocity: ray.Vector2,
+    radius: f32,
+    color: ray.Color,
+
+    fn render(self: *Ball) void {
+        ray.DrawCircleV(self.position, self.radius, self.color);
+    }
+
+    fn bounceFromEdges(self: *Ball) void {
+        if (self.position.y >= screen_height) {
+            ray.TraceLog(ray.LOG_INFO, "Game Over!");
+            ray.TraceLog(ray.LOG_INFO, "Score: %d", g_score);
+        }
+
+        const hit_left_edge = self.position.x <= ball_radius;
+        const hit_right_edge = self.position.x >= (screen_width - ball_radius);
+
+        if (hit_left_edge or hit_right_edge) {
+            self.velocity.x *= -1.0;
+            return;
+        }
+
+        if (self.position.y <= ball_radius) {
+            self.velocity.y *= -1.0;
+            return;
+        }
+    }
+
+    fn bounceFromBrick(self: *Ball) void {
+        for (&g_bricks) |*brick| {
+            if (ray.CheckCollisionCircleRec(self.position, ball_radius, brick.rectangle)) {
+                g_score += 1;
+                brick.rectangle.x = -200.0; // move enemy outside the screen
+                self.velocity.y *= -1.0;
+                return;
+            }
+        }
+    }
+
+    fn bounce(self: *Ball, player: *Player) void {
+        // world bounds
+        bounceFromEdges(self);
+
+        // bricks
+        bounceFromBrick(self);
+
+        // player
+        if (ray.CheckCollisionCircleRec(self.position, ball_radius, player.rectangle)) {
+            self.velocity.y *= -1.0;
+            return;
+        }
+    }
+
+    fn tick(self: *Ball, player: *Player) void {
+        self.position.x += self.velocity.x;
+        self.position.y += self.velocity.y;
+
+        self.bounce(player);
+    }
+};
 const Player = struct { rectangle: ray.Rectangle, color: ray.Color };
 const Brick = struct { rectangle: ray.Rectangle, color: ray.Color };
 
@@ -97,64 +158,8 @@ fn renderBricks() void {
     }
 }
 
-fn renderBall(ball: *Ball) void {
-    ray.DrawCircleV(ball.position, ball.radius, ball.color);
-}
-
 fn renderPlayer(player: *Player) void {
     ray.DrawRectangleRec(player.rectangle, player.color);
-}
-
-fn ballBounceFromEdges(ball: *Ball) void {
-    if (ball.position.y >= screen_height) {
-        ray.TraceLog(ray.LOG_INFO, "Game Over!");
-        ray.TraceLog(ray.LOG_INFO, "Score: %d", g_score);
-    }
-
-    const hit_left_edge = ball.position.x <= ball_radius;
-    const hit_right_edge = ball.position.x >= (screen_width - ball_radius);
-
-    if (hit_left_edge or hit_right_edge) {
-        ball.velocity.x *= -1.0;
-        return;
-    }
-
-    if (ball.position.y <= ball_radius) {
-        ball.velocity.y *= -1.0;
-        return;
-    }
-}
-
-fn ballBounceFromBrick(ball: *Ball) void {
-    for (&g_bricks) |*brick| {
-        if (ray.CheckCollisionCircleRec(ball.position, ball_radius, brick.rectangle)) {
-            g_score += 1;
-            brick.rectangle.x = -200.0; // move enemy outside the screen
-            ball.velocity.y *= -1.0;
-            return;
-        }
-    }
-}
-
-fn ballBounce(ball: *Ball, player: *Player) void {
-    // world bounds
-    ballBounceFromEdges(ball);
-
-    // bricks
-    ballBounceFromBrick(ball);
-
-    // player
-    if (ray.CheckCollisionCircleRec(ball.position, ball_radius, player.rectangle)) {
-        ball.velocity.y *= -1.0;
-        return;
-    }
-}
-
-fn ballTick(ball: *Ball, player: *Player) void {
-    ball.position.x += ball.velocity.x;
-    ball.position.y += ball.velocity.y;
-
-    ballBounce(ball, player);
 }
 
 pub fn main() !void {
@@ -183,7 +188,7 @@ pub fn main() !void {
         input(&player);
 
         // tick
-        ballTick(&ball, &player);
+        ball.tick(&player);
 
         // render
         ray.BeginDrawing();
@@ -192,7 +197,7 @@ pub fn main() !void {
         ray.ClearBackground(ray.BLACK);
 
         renderPlayer(&player);
-        renderBall(&ball);
+        ball.render();
         renderBricks();
 
         const score_string: [:0]u8 = try std.fmt.bufPrintZ(&score_buffer, "Score: {d}", .{g_score});
